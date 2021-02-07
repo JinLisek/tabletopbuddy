ae1870bdc61fe67c4f35c0642493abd7624f88b2
JinLisek <tastyramen@gmail.com>
2021-02-07 18:35:00 +0100

    frontend now sends players list in new game session (doesn't work yet, doesn't add to database even though 201 returned)

--- tabletopbuddy_frontend/src/AddPlay.js
+++ tabletopbuddy_frontend/src/AddPlay.js
@@ -2,54 +2,77 @@ import React from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 
+const AddedPlayer = (props) => {
+  return (
+    <Form.Group controlId="formAddedPlayer">
+      <Form.Label>Player name</Form.Label>
+      <Form.Control type="text" placeholder={props.playerName} readOnly />
+      <Button variant="primary">Remove</Button>
+    </Form.Group>
+  );
+};
+
 class AddPlay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { gameName: null, firstPlayer: null, secondPlayer: null, thirdPlayer: null };
+    this.state = { gameName: null, players: [] };
+    this.playerToBeAddedName = React.createRef("");
   }
+
+  validate = () => {
+    return this.state.gameName !== null && this.state.players.length > 0;
+  };
 
   addPlayToDatabase = (event) => {
     event.preventDefault();
-    axios.post("http://192.168.1.165:8000/game_sessions/", { game_name: "lol" });
-    console.log(this.state);
-    console.log("add to database");
+    if (this.validate()) {
+      const req = {
+        game_name: this.state.gameName,
+        players: this.state.players,
+      };
+      console.log("req: " + req.game_name + " " + req.players[0].name);
+      axios.post("http://192.168.1.165:8000/game_sessions/", req).then((resp) => {
+        console.log(resp);
+      });
+    } else {
+      alert("VALIDATION FAILED");
+    }
   };
 
   handleChangeGameName = (event) => {
     this.setState({ gameName: event.target.value });
   };
 
-  handleChangePlayerName = (id) => (event) => {
-    if (id === 1) {
-      this.setState({ firstPlayer: event.target.value });
-    } else if (id === 2) {
-      this.setState({ secondPlayer: event.target.value });
-    } else if (id === 3) {
-      this.setState({ thirdPlayer: event.target.value });
-    }
+  handleAddPlayer = (event) => {
+    console.log(this.playerToBeAddedName.current.value);
+
+    this.setState((state, _) => {
+      const newPlayers = [...state.players, { name: this.playerToBeAddedName.current.value }];
+      this.playerToBeAddedName.current.value = "";
+      return { players: newPlayers };
+    });
   };
 
   render = () => {
+    console.log(this.state);
     return (
       <Form onSubmit={this.addPlayToDatabase}>
         <Form.Group controlId="formGameName">
           <Form.Label>Game name</Form.Label>
           <Form.Control type="text" onChange={this.handleChangeGameName} placeholder="Enter game name" />
         </Form.Group>
-        <Form.Group controlId="formFirstPlayerName">
-          <Form.Label>First player name</Form.Label>
-          <Form.Control type="text" onChange={this.handleChangePlayerName(1)} placeholder="Enter first player name" />
-        </Form.Group>
-        <Form.Group controlId="formSecondPlayerName">
-          <Form.Label>Second second name</Form.Label>
-          <Form.Control type="text" onChange={this.handleChangePlayerName(2)} placeholder="Enter second player name" />
-        </Form.Group>
-        <Form.Group controlId="formThirdPlayerName">
-          <Form.Label>Third player name</Form.Label>
-          <Form.Control type="text" onChange={this.handleChangePlayerName(3)} placeholder="Enter third player name" />
+        {this.state.players.map((player, index) => {
+          return <AddedPlayer playerName={player.name} key={index} />;
+        })}
+        <Form.Group controlId="formAddPlayer">
+          <Form.Label>Player name</Form.Label>
+          <Form.Control type="text" ref={this.playerToBeAddedName} placeholder="Enter player name" />
+          <Button variant="primary" onClick={this.handleAddPlayer}>
+            Add Player
+          </Button>
         </Form.Group>
         <Button variant="primary" type="submit">
-          Add play
+          Add game session
         </Button>
       </Form>
     );
