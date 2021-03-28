f833dab44662c8754ca39eed4d23e288ad40a7ee
JinLisek <tastyramen@gmail.com>
2021-03-28 12:06:28 +0000

    Prettier NamedText.test.js

--- frontend/src/components/forms/__tests__/NamedText.test.js
+++ frontend/src/components/forms/__tests__/NamedText.test.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import NamedText from '../NamedText'
+import React from "react";
+import renderer from "react-test-renderer";
+import NamedText from "../NamedText";
 
 describe("NamedText tests", () => {
-    test("Default type for input should be text", () => {
-      const component = renderer.create(<NamedText />)
-      const tree = component.toJSON()
-      const [, input] = tree.children
-      expect(input.props['type']).toEqual("text")
-    });
-
-    test("Type from props should set on input", () => {
-        const component = renderer.create(<NamedText type="password"/>)
-        const tree = component.toJSON()
-        const [, input] = tree.children
-        expect(input.props['type']).toEqual("password")
-      });
+  test("Default type for input should be text", () => {
+    const component = renderer.create(<NamedText />);
+    const tree = component.toJSON();
+    const [, input] = tree.children;
+    expect(input.props["type"]).toEqual("text");
   });
-  
+
+  test("Type from props should set on input", () => {
+    const component = renderer.create(<NamedText type="password" />);
+    const tree = component.toJSON();
+    const [, input] = tree.children;
+    expect(input.props["type"]).toEqual("password");
+  });
+});
